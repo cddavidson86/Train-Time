e9711892c8e8012e916a7b842cd20ee4ec05901b
Charles Davidson <cddavidson86@yahoo.com>
2019-06-11 22:51:23 -0500

    adding main files

---
+++ assets/javascript/logic.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAY5bjChqi_ag3CUo8JI7skWsuWFvUrrYU",
+    authDomain: "train-times-20c72.firebaseapp.com",
+    databaseURL: "https://train-times-20c72.firebaseio.com",
+    projectId: "train-times-20c72",
+    storageBucket: "train-times-20c72.appspot.com",
+    messagingSenderId: "556536970294",
+    appId: "1:556536970294:web:6064112662b27f26"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
