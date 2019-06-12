1866d3746b6a8b96406a9998b41173675d917ca0
Charles Davidson <cddavidson86@yahoo.com>
2019-06-12 01:20:00 -0500

    inro into the basic js storage code

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,5 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
     apiKey: "AIzaSyAY5bjChqi_ag3CUo8JI7skWsuWFvUrrYU",
     authDomain: "train-times-20c72.firebaseapp.com",
     databaseURL: "https://train-times-20c72.firebaseio.com",
@@ -10,3 +10,29 @@
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+  var trainName = "";
+  var trainDestination = "";
+  var trainTime = "";
+  var trainFrequency = "";
+
+  $("#add-train").on("click", function() {
+    event.preventDefault();
+    trainName = $("#train-name").val().trim();
+    trainDestination = $("#train-destination").val().trim();
+    trainTime = $("#train-time").val().trim();
+    trainFrequency = $("#train-frequency").val().trim();
+
+    firebase.database().ref().set({
+        name: trainName,
+        destination: trainDestination,
+        time: trainTime,
+        frequency: trainFrequency
+    })
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(trainTime);
+    console.log(trainFrequency);
+  })
